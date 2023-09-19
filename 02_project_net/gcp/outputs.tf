c25ec51364dfc0d2d10b229721085aff27e5e43c
Junaid.Subhani <junaid.subhani@securekey.com>
2023-09-19 13:24:20 -0400

    Output net gcp proj number and name

--- 02_project_net/gcp/outputs.tf
+++ 02_project_net/gcp/outputs.tf
@@ -1,3 +1,11 @@
 output "net_proj_id" {
   value = module.net_project.project_id
 }
+
+output "net_proj_name" {
+  value = module.net_project.project_name
+}
+
+output "net_proj_number" {
+  value = module.net_project.project_number
+}
