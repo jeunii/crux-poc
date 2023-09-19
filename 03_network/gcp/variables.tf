52f8c835fd497a44e9d399e59276cb385f1d932f
Junaid.Subhani <junaid.subhani@securekey.com>
2023-09-19 12:50:05 -0400

    Add network and decouple network and svc project

---
+++ 03_network/gcp/variables.tf
@@ -0,0 +1,18 @@
+variable "org_id" {
+  type = string
+}
+
+variable "billing_id" {
+  type = string
+  default = "0146B8-6D3ADA-639083"
+}
+
+variable "net_project_apis" {
+  type    = list(string)
+  default = ["dns.googleapis.com", "servicenetworking.googleapis.com", "container.googleapis.com"]
+}
+
+variable "region" {
+  type    = string
+  default = "us-east4"
+}
