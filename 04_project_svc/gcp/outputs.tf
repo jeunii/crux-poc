db1837cd3ff843e993ec58e25f06d4e93733e988
Junaid.Subhani <junaid.subhani@securekey.com>
2023-09-20 10:40:47 -0400

    Add gcp svc project outputs

---
+++ 04_project_svc/gcp/outputs.tf
@@ -0,0 +1,11 @@
+output "svc_proj_id" {
+  value = module.svc_project.project_id
+}
+
+output "svc_proj_name" {
+  value = module.svc_project.project_name
+}
+
+output "svc_proj_number" {
+  value = module.svc_project.project_number
+}
