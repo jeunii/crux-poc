a2098969036a8d3079582b2ee415fa0570e7e2cf
Junaid.Subhani <junaid.subhani@securekey.com>
2023-09-27 10:04:42 -0400

    Add nodepool

--- 05_k8s/gcp/gke.tf
+++ 05_k8s/gcp/gke.tf
@@ -1,4 +1,5 @@
 module "kubernetes-engine" {
+  # https://registry.terraform.io/modules/terraform-google-modules/kubernetes-engine/google/latest
   source  = "terraform-google-modules/kubernetes-engine/google"
   version = "27.0.0"
   
@@ -17,4 +18,15 @@ module "kubernetes-engine" {
   release_channel           = "STABLE"
   kubernetes_version        = "1.27.3-gke.100"
   cluster_resource_labels   = { "mesh_id" : "proj-${data.tfe_outputs.svc-project-gcp.values.svc_proj_id}" }
+
+  node_pools = [
+    {
+      name                      = "default-node-pool"
+      machine_type              = "n2-standard-4"
+      node_locations            = "us-east1-a,us-east1-b"
+      min_count                 = 1
+      max_count                 = 6
+      spot                      = true
+    }
+  ]
 }
