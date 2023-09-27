9f5905a9ac068525a2580eaf63597adbcff16120
Junaid.Subhani <junaid.subhani@securekey.com>
2023-09-27 10:33:12 -0400

    Add ACM config

--- 06_anthos/gcp/main.tf
+++ 06_anthos/gcp/main.tf
@@ -15,6 +15,8 @@ module "acm" {
   enable_policy_controller  = false
   sync_repo                 = "git@github.com:jeunii/crux-poc.git"
   sync_branch               = "build"
+  sync_rev                  = "HEAD"
+  sync_wait_secs            = "15"
   policy_dir                = "asm_code_base/gcp/"
   secret_type               = "ssh"
   source_format             = "unstructured"
