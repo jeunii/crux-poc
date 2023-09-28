aa373811319fc0f86676b75b3f7010f014468e16
Junaid Subhani <jeunii@Junaids-MacBook-Pro.local>
2023-09-28 15:48:12 -0400

    depends_on

--- 07_anthos_asm/gcp/asm.tf
+++ 07_anthos_asm/gcp/asm.tf
@@ -10,6 +10,10 @@ resource "null_resource" "permissions" {
   provisioner "local-exec" {
     command = "chmod +x asmcli"
   }
+
+  depends_on = [
+    null_resource.download_asmcli
+  ]
 }
 
 resource "null_resource" "install" {
@@ -17,4 +21,8 @@ resource "null_resource" "install" {
   provisioner "local-exec" {
     command = "./asmcli validate --fleet_id svc-gcp-d349 --project_id svc-gcp-d349 --cluster_name svc-demo --output_dir /tmp --cluster_location us-east4"
   }
+  
+  depends_on = [
+    null_resource.permissions
+  ]
 }
