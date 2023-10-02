93db0ae289ebff214c682aa9d20a7f0f83fc898d
Lakshmanan Ganapathy - EXTERNAL <lakshmanan.onixnet.us>
2023-10-02 23:19:53 +0530

    Anthos config sync setup

---
+++ 06_anthos/on-prem/main.tf
@@ -0,0 +1,26 @@
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = "https://${var.on_prem_endpoint}"
+  token                  = var.token
+  cluster_ca_certificate = base64decode(var.on_prem_cluster_ca)
+}
+
+module "acm" {
+  # https://github.com/terraform-google-modules/terraform-google-kubernetes-engine/tree/master/modules/acm
+  source                    = "terraform-google-modules/kubernetes-engine/google//modules/acm"
+
+  project_id                = data.tfe_outputs.svc-project-on-prem.values.svc_proj_id
+  cluster_name              = "on-prem-k8s-cluster"
+  location                  = "us-east4-a"
+  enable_fleet_feature      = false
+  enable_policy_controller  = false
+  sync_repo                 = "git@github.com:jeunii/crux-poc.git"
+  sync_branch               = "build"
+  policy_dir                = "asm_code_base/on-prem/"
+  secret_type               = "ssh"
+  source_format             = "unstructured"
+  create_ssh_key            = true
+
+  create_metrics_gcp_sa = false
+}
